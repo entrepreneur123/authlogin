d871e24346707fa376724e9fd487784aa68e3bc6
@entrepreneur123 <irusha2056@gmail.com>
2022-11-16 19:09:13 +0545

    changes

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { SignupInput } from './dto/signup-input';
+import { UpdateAuthInput } from './dto/update-auth.input';
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+  create(signupInput: SignupInput) {
+    throw new Error('Method not implemented.');
+  }
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+  async signup(signupInput: SignupInput) {
+    const hashedPassword = await argon.hash(signupInput.password);
+    const user = await this.prisma.user.create({
+      data: {
+        name: signupInput.username,
+        hashedPassword: hashedPassword,
+        email: signupInput.email,
+      },
+    });
+    const { accessToken, refreshToken } = await this.createTokens(
+      user.id,
+      user.email,
+    );
+    await this.updateRefreshToken(user.id, refreshToken);
+    return { accessToken, refreshToken, user };
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthInput: UpdateAuthInput) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+
+  async createTokens(userId: number, email: string) {
+    const accessToken = this.jwtService.sign(
+      {
+        userId,
+        email,
+      },
+      {
+        expiresIn: '10s',
+        secret: this.configService.get('ACCESS_TOKEN_SECRET'),
+      },
+    );
+
+    const refreshToken = this.jwtService.sign(
+      {
+        userId,
+        email,
+        accessToken,
+      },
+      {
+        expiresIn: '7d',
+        secret: this.configService.get('REFRESH_TOKEN_SECRET'),
+      },
+    );
+    return { accessToken, refreshToken };
+  }
+  async updateRefreshToken(userId: number, refreshToken: string) {
+    const hashedRefreshedToken = await argon.hash(refreshToken);
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { hashedRefreshedToken },
+    });
+  }
+}
